898ac74244526e16e0721d438b9b934a21334016
EleisonC <ckalule7@gmail.com>
2022-06-07 14:36:37 +0300

    Initial Commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/EleisonC/K-Dogo/configs"
+	"github.com/EleisonC/K-Dogo/routes"
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterDogRoutes(r)
+	http.Handle("/", r)
+	// start the Database connection
+	configs.ConnectDB()
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
---
+++ models/dogModel.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"time"
+)
+
+// type DogVaccines struct {
+// 	Name string `json:"name,omitempty" validate:"required"`
+// 	VaccinePurpose string `json:"vaccinename,omitempty" validate:"required"`
+// 	DueDate time.Time `json:"duedate,omitempty" validate:"required"`
+// }
+
+type Dog struct {
+	Name string `bson:"name,omitempty" validate:"required"`
+	Breed string `bson:"breed,omitempty" validate:"required"`
+	DateOfBirth  time.Time `bson:"dateofbirth,omitempty" validate:"required"`
+	Sex string `bson:"sex,omitempty" validate:"required"`
+	// Vacination []DogVaccines `json:"vaccinations"`
+}
+
+type DogRequest struct {
+	Name string `json:"name,omitempty" validate:"required"`
+	Breed string `json:"breed,omitempty" validate:"required"`
+	DateOfBirth  string `json:"dateofbirth,omitempty" validate:"required"`
+	Sex string `json:"sex,omitempty" validate:"required"`
+	// Vacination []DogVaccines `json:"vaccinations"`
+}
+
+type UpdateDogRequest struct {
+	Name *string `json:"name,omitempty"`
+	Breed *string `json:"breed,omitempty"`
+	DateOfBirth  *time.Time `json:"dateofbirth,omitempty"`
+	Sex *string `json:"sex,omitempty"`
+	// Vacination []DogVaccines `json:"vaccinations"`
+}
+
